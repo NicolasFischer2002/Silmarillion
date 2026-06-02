bc4aa9452214d2cba41c8a2d344cf914ced87b43
NicolasFischer2002 <NicolasFischer49@gmail.com>
2026-06-02 20:20:25 -0300

    feat: AddPermissionToRoleCommandHandler finished

---
+++ Backend/Source/Contexts/AccessControlContext/Application/Roles/Commands/AddPermissionToRole/AddPermissionToRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Aggregates.Roles.Constants;
+
+namespace Application.Roles.Commands.AddPermissionToRole
+{
+    public sealed record AddPermissionToRoleCommand(
+        Guid RoleId,
+        PermissionCode Permission);
+}
---
+++ Backend/Source/Contexts/AccessControlContext/Application/Roles/Commands/AddPermissionToRole/AddPermissionToRoleCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Application.Abstractions.Persistence;
+using Domain.Aggregates.Roles.Errors;
+using SharedKernel.Results;
+
+namespace Application.Roles.Commands.AddPermissionToRole
+{
+    public sealed class AddPermissionToRoleCommandHandler
+    {
+        private readonly IRoleRepository _roleRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddPermissionToRoleCommandHandler(
+            IRoleRepository roleRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _roleRepository = roleRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> HandleAsync(
+            AddPermissionToRoleCommand command,
+            CancellationToken cancellationToken = default)
+        {
+            var role = await _roleRepository.GetByIdAsync(
+                command.RoleId,
+                cancellationToken);
+
+            if (role is null)
+                return Result.Failure(RoleErrors.RoleNotFound());
+
+            var addPermissionResult =
+                role.AddPermission(command.Permission);
+
+            if (addPermissionResult.IsFailure)
+                return Result.Failure(addPermissionResult.Errors);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
